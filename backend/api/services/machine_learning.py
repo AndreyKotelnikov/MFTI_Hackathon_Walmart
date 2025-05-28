096705abaa6d8800cceceaa3ae18575fc2b83500
lexgp <lexgp@yandex.ru>
2025-05-28 20:44:06 +0300

    Исправил баг для пустых результатов

--- backend/api/services/machine_learning.py
+++ backend/api/services/machine_learning.py
@@ -107,7 +107,7 @@ def process_predictions_with_ml(research):
     ]
 
     # 5. Среднее всех ratio
-    average_ratio = sum(item['ratio'] for item in ratio_dicts) / len(ratio_dicts)
+    average_ratio = sum(item['ratio'] for item in ratio_dicts) / len(ratio_dicts) if len(ratio_dicts) else 1
 
     # Вывод результатов
     # print(f"1. Сумма реальных продаж: {sum_real}")
--- frontend/src/views/dashboard/TopRatingItems.vue
+++ frontend/src/views/dashboard/TopRatingItems.vue
@@ -4,7 +4,7 @@
     
     <v-card-text class="d-flex gap-4 flex-wrap w-full" style="justify-content: space-around;">
       <!-- Топ-5 лучших -->
-      <div class="mb-6" style="max-width: 600px; min-width: 300px; flex: 1;">
+      <div v-if="topItems.length" class="mb-6" style="max-width: 600px; min-width: 300px; flex: 1;">
         <div class="text-subtitle-1 text-white mb-2">Топ-5 роста продаж</div>
 
         <div v-for="(item, index) in topItems" :key="index" class="mb-3 text-caption">
@@ -25,7 +25,7 @@
         </div>
       </div>
 
-      <div class="mb-6" style="max-width: 600px; min-width: 300px; flex: 1;">
+      <div v-if="bottomItems.length" class="mb-6" style="max-width: 600px; min-width: 300px; flex: 1;">
         <div class="text-subtitle-1 text-white mb-2">Топ-5 падения продаж</div>
 
         <div v-for="(item, index) in bottomItems" :key="index" class="mb-3 text-caption">
@@ -47,7 +47,12 @@
       </div>
     </v-card-text>
 
-    <v-card-text>
+
+    <v-card-text class="mb-4 text-warning" v-if="!topItems.length && !bottomItems.length">
+      Вероятно все предсказания были отсеяны из-за низкой достоверности. Попробуйте более общирное исследование.
+    </v-card-text>
+
+    <v-card-text v-else>
 
       <!-- Легенда -->
       <div class="mt-4 text-caption text-medium-emphasis">
